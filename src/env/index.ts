3b80bcca8326d372747e852c0fdd91ab268a1bd0
gabrielLaino <lainogabriel14@gmail.com>
2025-03-10 08:46:50 -0300

    finalizando configuracoes

---
+++ src/env/index.ts
@@ -0,0 +1,11 @@
+import 'dotenv/config';
+import { z } from 'zod';
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
+  PORT: z.coerce.number().default(3001)
+});
+
+const env = envSchema.parse(envSchema);
+
+export default env;
